d792de9e3e9c2aa203f116ed7f67f56cbc2c353c
Aldo Gomez <gomezfx@gmail.com>
2020-01-27 23:25:16 -0800

    fixed name

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var RelayPin = new Gpio(26, 'out'); //declare GPIO4 an output
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/on', (req, res) => LEDPin.writeSync(1))
+app.get('/on', (req, res) => RelayPin.writeSync(1))
 
-app.get('/off', (req, res) => LEDPin.writeSync(0))
+app.get('/off', (req, res) => RelayPin.writeSync(0))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
